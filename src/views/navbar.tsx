293ecc3671d9ed00b64c02d2923e0a131ef2c5b7
thujuli <thujuli037@gmail.com>
2024-04-14 07:37:22 +0800

    refactor(navbar): Enhance interactivity when select a menu

--- src/views/navbar.tsx
+++ src/views/navbar.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import logo from "@/public/images/logo.png";
 import { menuItems } from "@/lib/helper";
 import NavbarMenu from "@/components/navbar-menu";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const Navbar: React.FC = () => {
+  const [menuActive, setMenuActive] = useState("");
+
   return (
     <header className="relative h-[50px] md:h-[100px]">
       <nav className="fixed z-50 flex h-[50px] w-full items-center justify-between bg-black px-5 text-white md:h-[100px] lg:px-[70px]">
@@ -27,9 +32,19 @@ const Navbar: React.FC = () => {
           height={100}
           className="hidden object-contain md:inline"
         />
-        <div className="hidden text-gray md:gap-3 lg:flex lg:text-xl xl:gap-10 xl:text-2xl">
+        <div className="hidden text-gray lg:flex lg:text-xl xl:gap-2 xl:text-2xl">
           {menuItems.map((item, idx) => (
-            <Link key={idx} href={item.link}>
+            <Link
+              key={idx}
+              href={item.link}
+              onClick={() => setMenuActive(item.name)}
+              className={cn(
+                "rounded-full p-2 hover:bg-green hover:font-bold hover:text-black xl:px-4",
+                menuActive === item.name
+                  ? "rounded-full bg-green p-2 font-bold text-black xl:px-4"
+                  : "",
+              )}
+            >
               {item.name}
             </Link>
           ))}
